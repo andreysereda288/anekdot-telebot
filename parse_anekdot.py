a4de5992739306d68df3f510192f3a61b8dde0d3
andreysereda288 <andreysereda288@gmail.com>
2023-01-30 00:53:52 +0400

    contants

--- parse_anekdot.py
+++ parse_anekdot.py
@@ -4,11 +4,14 @@ from bs4 import BeautifulSoup
 URL_ANEK = 'https://www.anekdot.ru/random/anekdot/'
 URL_STORY = 'https://www.anekdot.ru/last/story/'
 
+ANEKDOT_TYPE = 'anekdots'
+STORY_TYPE = 'stories'
+
 
 def parse_anekdots(content_type='/anekdots'):
-    if content_type == '/anekdots':
+    if content_type == ANEKDOT_TYPE:
         url = URL_ANEK
-    elif content_type == '/stories':
+    elif content_type == STORY_TYPE:
         url = URL_STORY
     else:
         return 'No content'
@@ -20,14 +23,14 @@ def parse_anekdots(content_type='/anekdots'):
         final_str = ''
         for div in topic_divs:
             if div.has_attr('data-id'):
-                if content_type == '/anekdots':
+                if content_type == ANEKDOT_TYPE:
                     date = div.find('p', class_='title')
                     final_str += date.text + '\n'
                 text = div.find('div', class_='text')
                 text = str(text).replace('<div class="text">', '').replace('</div>', '')
                 text = text.replace('<br/>', '\n')
                 final_str += text + 2*'\n'
-                if content_type == '/stories':
+                if content_type == STORY_TYPE:
                     final_str += '-------\n'
     except:
         final_str = 'Something went wrong'
--- telebot1.py
+++ telebot1.py
@@ -3,7 +3,7 @@ import os
 import telebot
 from telebot import types, util
 
-from parse_anekdot import parse_anekdots
+from parse_anekdot import parse_anekdots, ANEKDOT_TYPE, STORY_TYPE
 
 token = os.environ.get('API_TELEBOT', '')
 
@@ -11,8 +11,8 @@ bot = telebot.TeleBot(token)
 
 markup = types.InlineKeyboardMarkup()
 markup.row_width = 2
-markup.add(types.InlineKeyboardButton("Анекдоты", callback_data="anekdots"),
-           types.InlineKeyboardButton("Истории", callback_data="stories"),
+markup.add(types.InlineKeyboardButton("Анекдоты", callback_data=ANEKDOT_TYPE),
+           types.InlineKeyboardButton("Истории", callback_data=STORY_TYPE),
            types.InlineKeyboardButton("Raise error", callback_data="error"),
            )
 
@@ -24,17 +24,15 @@ def send_welcome(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
-    if call.data == "anekdots":
+    if call.data == 'error':
+        raise RuntimeError()
+    else:
         bot.answer_callback_query(call.id, "Loading...")
-        content = parse_anekdots()
+        content = parse_anekdots(f'/{call.data}')
         splitted_text = util.smart_split(content, chars_per_string=3000)
         for text in splitted_text:
             bot.send_message(call.from_user.id, text)
         bot.send_message(call.from_user.id, "Choose type:", reply_markup=markup)
-    elif call.data == "stories":
-        bot.answer_callback_query(call.id, "Not support so far")
-    if call.data == 'error':
-        raise RuntimeError()
 
 
 @bot.message_handler(commands=['test'])
