90372055d7a88d07dc5aaf5549e979f2aa5d1126
andreysereda288 <andreysereda288@gmail.com>
2023-01-29 16:54:08 +0400

    initial version

---
+++ parse_anekdot.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL_ANEK = 'https://www.anekdot.ru/random/anekdot/'
+URL_STORY = 'https://www.anekdot.ru/last/story/'
+
+
+def parse_anekdots(content_type='/anekdots'):
+    if content_type == '/anekdots':
+        url = URL_ANEK
+    elif content_type == '/stories':
+        url = URL_STORY
+    else:
+        return 'No content'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, features="lxml")
+
+    topic_divs = soup.find_all('div', class_='topicbox')
+    try:
+        final_str = ''
+        for div in topic_divs:
+            if div.has_attr('data-id'):
+                data = div.find('p', class_='title')
+                final_str += data.text + '\n'
+                text = div.find('div', class_='text')
+                text = str(text).replace('<div class="text">', '').replace('</div>', '')
+                text = text.replace('<br/>', '\n')
+                final_str += text + 2*'\n'
+    except:
+        final_str = 'Something went wrong'
+    return final_str
---
+++ telebot1.py
@@ -0,0 +1,42 @@
+import os
+
+import telebot
+from telebot import types, util
+
+from parse_anekdot import parse_anekdots
+
+token = os.environ.get('API_TELEBOT', '')
+
+bot = telebot.TeleBot(token)
+
+markup = types.InlineKeyboardMarkup()
+markup.row_width = 2
+markup.add(types.InlineKeyboardButton("Анекдоты", callback_data="anekdots"),
+           types.InlineKeyboardButton("Истории", callback_data="stories"),
+           )
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "Choose type:", reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == "anekdots":
+        bot.answer_callback_query(call.id, "Loading...")
+        content = parse_anekdots()
+        splitted_text = util.smart_split(content, chars_per_string=3000)
+        for text in splitted_text:
+            bot.send_message(call.from_user.id, text)
+        bot.send_message(call.from_user.id, "Choose type:", reply_markup=markup)
+    elif call.data == "stories":
+        bot.answer_callback_query(call.id, "Not support so far")
+
+
+@bot.message_handler(commands=['test'])
+def send_welcome(message):
+    bot.reply_to(message, "works")
+
+
+bot.infinity_polling()
